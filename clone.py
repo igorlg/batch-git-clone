824b31ad1f7244032847997e41857fc9fe3150b7
Igor Gentil <igor.lg@gmail.com>
2016-09-07 11:05:02 +1200

    Refactored

--- clone.py
+++ clone.py
@@ -1,15 +1,20 @@
 import yaml, subprocess, os
 
+def git_clone(name, path):
+	if not os.path.exists(path):
+		print "Cloning %s into %s" % (name, path)
+		subprocess.call(["git", "clone", v, path])
+		print "Done!"
+
 def recursive_walk(d, depth=0, parent=[]):
 	for k, v in sorted(d.items(), key=lambda x: x[0]):
 		if isinstance(v, dict):
 			recursive_walk(v, depth+1, parent + [k])
 		else:
 			path = "/".join(parent + [k])
-			if not os.path.exists(path):
-				subprocess.call(["git", "clone", v, path])
+			git_clone(v, path)			
 
-with open('repos.yml', 'r') as f:
+with open('_repos.yml', 'r') as f:
 	repos = yaml.load(f)
 
 recursive_walk(repos)
