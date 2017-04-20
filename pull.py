ff72f2c25739474fa172acd5f7297b97f1098f76
Igor Gentil <igor.lg@gmail.com>
2017-04-20 14:34:04 +1200

    Refactoring Pull script

--- pull.py
+++ pull.py
@@ -2,14 +2,43 @@ from joblib import Parallel, delayed
 import multiprocessing
 import yaml, subprocess, os
 
+def git(commands, path):
+  try:
+    cmd = ['git'] + commands
+  except:
+    cmd = ['git'] + [commands]
+
+  run = subprocess.Popen(cmd,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         cwd=path)
+  run.wait()
+  return run
+
+def filter_output(line):
+  if (
+    not line.startswith('Warning: Permanently added')
+    and not line.startswith('See git-pull(1) for details.') 
+    and not line.startswith('git pull <remote> <branch>')
+  ):
+    print line.strip()
+
+def success(path, run):
+  for l in run.stdout: filter_output(l)
+  print "Done pulling " + path
+
+def failure(path, run):
+  for l in run.stderr: filter_output(l)
+  print "Failed to pull " + path
+
 def git_pull(path):
-  basepath = os.path.dirname(os.path.abspath(__file__))
-  if os.path.exists(path):
-    os.chdir(path)
-    print "Pulling " + path
-    subprocess.call(["git", "pull"])
-    # print "Done!"
-    os.chdir(basepath)
+  print "Pulling " + path
+  pull = git('pull', path)
+
+  if pull.returncode == 0:
+    success(path, pull)
+  else:
+    failure(path, pull)
 
 def recursive_walk(d, depth=0, parent=[]):
   for k, v in sorted(d.items(), key=lambda x: x[0]):
@@ -17,8 +46,8 @@ def recursive_walk(d, depth=0, parent=[]):
       recursive_walk(v, depth+1, parent + [k])
     else:
       path = "/".join(parent + [k])
-      paths.append(path)
-      # git_pull(path)
+      if os.path.exists(path):
+        paths.append(path)
 
 with open('_repos.yml', 'r') as f:
   repos = yaml.load(f)
