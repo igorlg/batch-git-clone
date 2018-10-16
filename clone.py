e6aaa031bc0ae6d7f267150f4080e0400246ae16
Igor Gentil <igor.lg@gmail.com>
2018-10-17 08:24:41 +1300

    Adding symlink support on Clone, adding back Pull

--- clone.py
+++ clone.py
@@ -18,14 +18,31 @@ def git_clone(name, path):
   else:
     failure(path)
 
+def create_symlinks(links):
+  for t,l in links.iteritems():
+    if os.path.exists(l): # Link already exists
+      continue
+    elif not os.path.exists(t): # Targer doesn't exist
+      print("Skipping link %s to %s as %s doesn't exists" % (l, t, t))
+      continue
+
+    print("Creating symlink %s to %s" % (l, t))
+    os.symlink(t, l)
+
 def recursive_walk(d, depth=0, parent=[]):
   for k, v in sorted(d.items(), key=lambda x: x[0]):
     if isinstance(v, dict):
       recursive_walk(v, depth+1, parent + [k])
     else:
       path = "/".join(parent + [k])
-      if not os.path.exists(path):
-        git_repos[v] = path
+      try:
+        if v.startswith('symlink:'):
+          link = v.split(':')[1]
+          sym_links[link] = path
+        elif not os.path.exists(path):
+          git_repos[v] = path
+      except:
+        continue
 
 def main():
   with open('_repos.yml', 'r') as f:
@@ -33,6 +50,8 @@ def main():
 
   recursive_walk(repos)
   Parallel(n_jobs=get_cpu_count())(delayed(git_clone)(r,p) for r,p in git_repos.iteritems())
+  create_symlinks(sym_links)
 
 git_repos = {}
+sym_links = {}
 main()
